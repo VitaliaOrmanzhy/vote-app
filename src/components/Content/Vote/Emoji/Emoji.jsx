a2c1fdcce5e67d6f02277274aa419a2987469297
VitaliaOrmanzhy <vormanzi812@gmail.com>
2024-12-31 23:21:53 +0200

    new

--- src/components/Content/Vote/Emoji/Emoji.jsx
+++ src/components/Content/Vote/Emoji/Emoji.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Emoji(props) {
-  return (
-    <div>
-      <button onClick={() => props.onButtonClick(props)}>
-        <img src="" />
-      </button>
-      <p>{props.value}</p>
-    </div>
-  );
+export class Emoji extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  onButtonClick() {
+    this.setState((state) => {
+      return {
+        count: state.count + 1,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>
+          <img src={this.props.img} alt="" />
+        </button>
+        <p>{this.state.count}</p>
+      </div>
+    );
+  }
 }
--- src/components/Content/Vote/Vote.jsx
+++ src/components/Content/Vote/Vote.jsx
@@ -1,44 +1,13 @@
 import React, { Component } from "react";
-import Emoji from "./Emoji/Emoji";
+import { Emoji } from "./Emoji/Emoji";
 
 export class Vote extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      voted: {
-        1: 0,
-        2: 0,
-        3: 0,
-      },
-    };
-
-    this.onButtonClick = this.onButtonClick.bind(this);
-  }
-
-  onButtonClick(index) {
-    this.setState((state) => {
-      return {
-        voted: {
-          ...this.state.voted,
-          [index]: this.state.voted[index] + 1,
-        },
-      };
-    });
-  }
-
   render() {
     return (
       <div className="vote">
-        {Object.entries(this.state.voted).map((el) => {
-          //[[1, 2], [2, 3]]
-          return (
-            <Emoji
-              index={el[0]}
-              value={el[1]}
-              onButtonClick={this.onButtonClick}
-            />
-          );
-        })}
+        <Emoji img="" />
+        <Emoji img="" />
+        <Emoji img="" />
       </div>
     );
   }
