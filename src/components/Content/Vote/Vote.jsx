8bec5960acbe5792e2fa0f6c45b10bd616060319
VitaliaOrmanzhy <vormanzi812@gmail.com>
2025-01-02 20:35:16 +0200

    2 commit

--- src/components/Content/Vote/Vote.jsx
+++ src/components/Content/Vote/Vote.jsx
@@ -4,15 +4,30 @@ import Emoji from "./Emoji/Emoji";
 export class Vote extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       voted: {
-        1: 0,
-        2: 0,
-        3: 0,
+        1: {
+          img: "",
+          count: 0,
+        },
+        2: {
+          img: "",
+          count: 0,
+        },
+        3: {
+          img: "",
+          count: 0,
+        },
       },
     };
 
     this.onButtonClick = this.onButtonClick.bind(this);
+  }
+
+  getResults() {
+    //Вот тут найти наибольший count
+    //Записать его в стейт
   }
 
   onButtonClick(index) {
@@ -20,25 +35,34 @@ export class Vote extends Component {
       return {
         voted: {
           ...this.state.voted,
-          [index]: this.state.voted[index] + 1,
+          [index]: {
+            img: this.state.voted[index].img,
+            count: this.state.voted[index] + 1,
+          },
         },
       };
     });
   }
 
   render() {
+    //Вот тут измени цикл, чтобы и count передавался, и картинка
     return (
       <div className="vote">
         {Object.entries(this.state.voted).map((el) => {
           //[[1, 2], [2, 3]]
           return (
             <Emoji
+              //img={ сюда картинку }
               index={el[0]}
               value={el[1]}
               onButtonClick={this.onButtonClick}
             />
           );
         })}
+
+        <button onClick={this.getResults}></button>
+
+        <p>The biggest count: {this.state.theBiggest}</p>
       </div>
     );
   }
